ca9e2735d2f0916b5b0a98f3310434a6b9eb7069
jayvanandel07 <j.vanandel07@gmail.com>
2023-03-08 21:32:41 +0530

    Home Structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import Link from "next/link";
 import styles from "styles/Home.module.css";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
+    <>
       {/* <Link href={"/login"}>Login</Link>
-      <Link href={"/register"}>Register</Link>
-      <Link href={"/products"}>Products</Link>
-      <Link href={"/cart"}>Cart</Link> */}
-      <div className={styles.main}>Home</div>
-    </div>
+    //   <Link href={"/register"}>Register</Link>
+    //   <Link href={"/products"}>Products</Link>
+    // <Link href={"/cart"}>Cart</Link> */}
+      <div className={styles.main}>
+        <div className={styles.intro}>
+          <h2>
+            Your health is our business. So the choice is yours to make you
+            healthy.
+          </h2>
+        </div>
+
+        <div className={styles.STD}>
+          <Image
+            src={require("public/Icons/Arrow.svg")}
+            alt="arrow down icon"
+          />
+          <h4>Scroll to discover</h4>
+        </div>
+      </div>
+      <div className={styles.wrapper}>
+        <Image src={require("public/Assets/Bowl.svg")} alt="bowl of grains" />
+      </div>
+    </>
   );
 }
