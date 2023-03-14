e920d09aeada8e2aa8589d84a9884bb7adfd6ec9
jayvanandel07 <j.vanandel07@gmail.com>
2023-03-14 20:11:37 +0530

    pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Link from "next/link";
 import styles from "styles/Home.module.css";
 import Image from "next/image";
 import useMousePosition from "hooks/useMousePosition";
 
 export default function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
+
   const { x, y } = useMousePosition();
-  console.log(x, y);
+  const paginationHandler = (page: any) => {
+    setCurrentPage(page);
+  };
   return (
     <>
       {/* <Link href={"/login"}>Login</Link>
@@ -42,6 +47,56 @@ export default function Home() {
             translate: `${x}px ${y}px`,
           }}
         />
+        <div className={styles.pagination}>
+          <div
+            className={`${styles.page_dot} ${
+              currentPage == 1 && styles.active
+            }`}
+            onClick={() => paginationHandler(1)}
+          ></div>
+          <div
+            className={`${styles.page_dot} ${
+              currentPage == 2 && styles.active
+            }`}
+            onClick={() => paginationHandler(2)}
+          ></div>
+          <div
+            className={`${styles.page_dot} ${
+              currentPage == 3 && styles.active
+            }`}
+            onClick={() => paginationHandler(3)}
+          ></div>
+          <div
+            className={`${styles.page_dot} ${
+              currentPage == 4 && styles.active
+            }`}
+            onClick={() => paginationHandler(4)}
+          ></div>
+          <div
+            className={`${styles.page_dot} ${
+              currentPage == 5 && styles.active
+            }`}
+            onClick={() => paginationHandler(5)}
+          ></div>
+          <div
+            className={`${styles.page_dot} ${
+              currentPage == 6 && styles.active
+            }`}
+            onClick={() => paginationHandler(6)}
+          ></div>
+          <div
+            className={`${styles.page_dot} ${
+              currentPage == 7 && styles.active
+            }`}
+            onClick={() => paginationHandler(7)}
+          ></div>
+          <div
+            className={`${styles.page_dot} ${
+              currentPage == 8 && styles.active
+            }`}
+            onClick={() => paginationHandler(8)}
+          ></div>
+        </div>
       </div>
     </>
   );
