ac78efc67532c3463abce42bbcd0e22190d12825
jayvanandel07 <j.vanandel07@gmail.com>
2023-03-17 10:51:59 +0530

    navbar and ts fix

--- components/common/form-field-array/index.tsx
+++ components/common/form-field-array/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 
-const FieldArray = ({ control }) => {
+const FieldArray = ({ control }: any) => {
   const {
     fields,
     append: appendIngredient,
