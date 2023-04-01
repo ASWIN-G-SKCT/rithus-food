5673b316c4f78ca6bbfd6b4eb5a10a7f20daeb33
jayvanandel07 <j.vanandel07@gmail.com>
2023-04-01 21:38:33 +0530

    menu base

--- components/common/navbar/index.tsx
+++ components/common/navbar/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import Image from "next/image";
 import styles from "./navbar.module.css";
@@ -8,19 +8,51 @@ import styles from "./navbar.module.css";
 const Navbar = () => {
   const { user } = useSelector((state: any) => state?.auth);
   const { pathname } = useRouter();
+
+  const [menu, setMenu] = useState(true);
+
   return (
-    <div className={styles.navbar}>
-      <div className={styles.logo}>
-        <h1>Rithus</h1>
-        <h1>Foods</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.navbar}>
+        <div className={styles.logo}>
+          <h1>Rithus</h1>
+          <h1>Foods</h1>
+        </div>
+        {pathname === "/" && user?.admin && <Link href={"/admin"}>Admin</Link>}
+        <div className={styles.actions}>
+          <h3 className={styles.moms_choice}>Mom's Choice</h3>
+          <Image src={require("public/Icons/Search.svg")} alt="Search Icon" />
+          <Image src={require("public/Icons/User.svg")} alt="User Icon" />
+          <Image src={require("public/Icons/Cart.svg")} alt="Cart Icon" />
+          <Image
+            src={require("public/Icons/Burger.svg")}
+            alt="Burger Icon"
+            onClick={() => setMenu(!menu)}
+          />
+        </div>
       </div>
-      {pathname === "/" && user?.admin && <Link href={"/admin"}>Admin</Link>}
-      <div className={styles.actions}>
-        <h3 className={styles.moms_choice}>Mom's Choice</h3>
-        <Image src={require("public/Icons/Search.svg")} alt="Search Icon" />
-        <Image src={require("public/Icons/User.svg")} alt="User Icon" />
-        <Image src={require("public/Icons/Cart.svg")} alt="Cart Icon" />
-        <Image src={require("public/Icons/Burger.svg")} alt="Burger Icon" />
+      <div className={`${styles.menu} ${menu && styles.active}`}>
+        <h2
+          style={{
+            transform: `translateY(${100 * (menu ? 0 : 100)}%)`,
+          }}
+        >
+          Products
+        </h2>
+        <h2
+          style={{
+            transform: `translateY(${100 * (menu ? 0 : 100)}%)`,
+          }}
+        >
+          Founder's Note
+        </h2>
+        <h2
+          style={{
+            transform: `translateY(${100 * (menu ? 0 : 100)}%)`,
+          }}
+        >
+          Contact Us
+        </h2>
       </div>
     </div>
   );
