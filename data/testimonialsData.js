a81936c48108b08f3b5d2e6316f232b79df7a589
jayvanandel07 <j.vanandel07@gmail.com>
2023-05-05 18:39:06 +0530

    testimonial data

--- data/testimonialsData.js
+++ data/testimonialsData.js
@@ -2,35 +2,40 @@ const testimonialsData = [
   {
     "name": "Mrs.Sowbarnika",
     "city": "Banglore",
-    "media": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/Sowbarnika.jpeg'],
+    "photos": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/Sowbarnika.jpeg'],
+    "videos": [],
     "rating": 5,
     "review": "They offer a great variety of products to try and explore, which is perfect for those who enjoy experimenting with new foods."
   },
   {
     "name": "Mrs.Thahaaseen",
     "city": "Salem",
-    "media": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/Thahaaseen.jpeg'],
+    "photos": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/Thahaaseen.jpeg'],
+    "videos": [],
     "rating": 5,
     "review": "They have the best food product in the market."
   },
   {
     "name": "Dr.Keerthana",
     "city": "Tirupur",
-    "media": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/Dr+Keerthana.jpeg'],
+    "photos": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/Dr+Keerthana.jpeg'],
+    "videos": [],
     "rating": 4,
     "review": "My son likes the food they make."
   },
   {
     "name": "Mrs.Jamuna",
     "city": "Coimbatore",
-    "media": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/jamuna.jpeg'],
+    "photos": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/jamuna.jpeg'],
+    "videos": [],
     "rating": 4,
     "review": "The food products that they offered had excellent quality, with ingredients that were fresh, flavorful, and free from preservatives or artificial additives 😁. "
   },
   {
     "name": "Mrs.Shaleena",
     "city": "Hosur",
-    "media": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/Shaleena.mp4'],
+    "photos": [],
+    "videos": ['https://rithusfoods.s3.ap-south-1.amazonaws.com/Assets/testimonial+data/Shaleena.mp4'],
     "rating": 5,
     "review": "Yummiest food i ever tasted 🤤."
 
